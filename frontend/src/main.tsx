7f8a6658ade6031a06f9d3eea3341c869a1a4b4e
James Propert <james.propert@sixpivot.com.au>
2025-01-18 13:14:49 +1000

    Add basic routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+import "./index.css";
+
+const root = document.getElementById("root")!;
+
+createRoot(root).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
