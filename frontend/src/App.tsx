683ab172b05cb28441a9f7337339d529009b35ba
James Propert <james.propert@sixpivot.com.au>
2025-01-18 11:04:44 +1000

    Add basic login/logout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
+import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { useState } from "react";
+import Loading from "./components/Loading";
 import "./App.css";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,9 +14,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <LogoutButton />
       </div>
     </>
   );
 }
 
-export default App;
+export default withAuthenticationRequired(App, {
+  onRedirecting: () => <Loading />,
+});
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
@@ -9,10 +10,18 @@ const root = document.getElementById("root")!;
 
 createRoot(root).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-bp23xcr1kyeb3ru3.us.auth0.com"
+      clientId="rY5YUnUmY5SiIfya1VJjrnzeP7KDa4ON"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Auth0Provider>
   </StrictMode>,
 );
